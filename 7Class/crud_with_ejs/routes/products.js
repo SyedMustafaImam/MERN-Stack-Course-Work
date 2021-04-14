8bc2d19d2aa15b7addd2251c0caa17bee3620f57
SyedMustafaImam <cs1812134@szabist.pk>
2021-04-15 02:34:52 +0500

    Class 8 Login anand session management

--- 7Class/crud_with_ejs/routes/products.js
+++ 7Class/crud_with_ejs/routes/products.js
@@ -6,7 +6,8 @@ var product_controller = require('../controllers/product.controller');
 router.get('/list', product_controller.list);
 router.get('/createform', product_controller.formcreate);
 router.post('/create', product_controller.create);
-router.put('/update', product_controller.update);
-router.post('/delete', product_controller.delete);
+router.post('/:id/delete', product_controller.delete);
+router.put('/updateform/:id', product_controller.update_form);
+router.post('/:id/update', product_controller.update)
 
 module.exports = router;
