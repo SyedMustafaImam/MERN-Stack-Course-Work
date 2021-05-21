a6c68ba6ccbf6395332efa17d31ba0da14538527
SyedMustafaImam <cs1812134@szabist.pk>
2021-05-21 14:55:28 +0500

    React app auto update

--- 7Class/crud_with_ejs/models/product.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-let PoductSchema = new Schema({
-    name:{type:String, required:true, max:100},
-    price:{type:Number, required:true}
-})
-
-module.exports = mongoose.model('Poduct', PoductSchema )
--- 8Class/myapp/models/product.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-
-let ProductSchema=new Schema({
-    name:{type:String, required:true,max:100},
-    price:{type:Number,required:true}
-});
-
-module.exports=mongoose.model('Product',ProductSchema);
